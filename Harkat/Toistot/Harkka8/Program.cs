440d8b119de9060be23baf925d8266e7359a714f
Tapio Suominen <suominen.tapio@outlook.com>
2023-02-18 21:32:28 +0200

    lisää harkkoja

--- Harkat/Toistot/Harkka8/Program.cs
+++ Harkat/Toistot/Harkka8/Program.cs
@@ -1,1 +1,10 @@
 ﻿
+string[] array = { "ei", "terve", "halojaa", "vittu", "ei", "saatana", "jumalauta", "samk", "automaatio", "tarkov", "huomenta" };
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length > 2)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
---
+++ Harkat/Toistot/Harkka9/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int[] nums = new int[10];
+
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = i;
+}
+for (int i = 0; i < nums.Length; i += 2)
+{
+    Console.WriteLine(nums[i] + " " + nums[i+1]);
+}
